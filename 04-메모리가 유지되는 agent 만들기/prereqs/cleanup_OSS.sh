774302c82cef05eea37e3a4ce8fa2a09eeb646fb
chloe-kwak <karmax7@gmail.com>
2025-10-04 17:17:07 +0000

    Clean up duplicate folders and update local changes

--- 04-메모리가 유지되는 agent 만들기/prereqs/cleanup_OSS.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# clean up opensearch serverless resources
-echo "Removing Opensearch Serverless resources ..."
-python prereqs/opensearch.py --mode delete
--- 04-메모리가 유지되는 agent 만들기/prereqs/deploy_OSS.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Deploy OpenSearch Serverless resources
-echo "deploying opensearch Serverless ..."
-python prereqs/opensearch.py --mode create
